a0a1430e121f3a2fa6eed46e165365555b24e4ae
atugharajohn <atugharajohn@gmail.com>
2023-10-08 17:23:11 +0530

    operations in js

--- 03_typeConversion.js
+++ 03_typeConversion.js
@@ -1,10 +1,10 @@
 // type conversion in JS
 let score = "34";
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "34" => 34
 // "34abc" => NaN
@@ -12,11 +12,36 @@ console.log(valueInNumber);
 
 let isLoogedIn = 1;
 let booleanIsLoogedIn = Boolean(isLoogedIn);
-console.log(booleanIsLoogedIn);
+// console.log(booleanIsLoogedIn);
 
 // "" => false
 // "yash" => true
 
 let num = 23;
-console.log(Number(num));
-console.log(typeof(num)); 
+// console.log(Number(num));
+// console.log(typeof(num)); 
+
+// ************** Operations in js **************
+
+let value = 4;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2*2);
+// console.log(3+2);
+// console.log(1-2);
+// console.log(10/2);
+// console.log(4%2);
+
+let str1 = "yash";
+let str2 = "pal";
+// console.log(str1 + str2);
+
+// console.log("1" + 2); // 12 => (input ,preferType)
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 3); // 123
+// console.log(1 + 2 + "3"); //33
+
+// let num1 , num2 , num3;
+// num1 = num2 = num3 = 3+1;
+// console.log(num1);
