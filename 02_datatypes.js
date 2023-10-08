dbb1809381fd615725b72fbd5beeab8fc9b67ff0
atugharajohn <atugharajohn@gmail.com>
2023-10-08 16:50:26 +0530

    datatypes in javscript

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat as js code to newer version
+
+console.log(2 + 2);
+
+let name = "yash" // string datatype
+let age = 18; // number
+let isLoggedIn = false; // boolean
+ 
+console.log(typeof(age));
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined 
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value , it is object
+// undefined =>
+// symbol => unique
+
+// object
