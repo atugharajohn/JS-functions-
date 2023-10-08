6182675a46a9bdb2f75a38be5b6ec1b1c75449ab
atugharajohn <atugharajohn@gmail.com>
2023-10-08 17:33:14 +0530

    comparsion of datatypes in js

---
+++ 04_comparsion.js
@@ -0,0 +1,20 @@
+// comparsion of datatypes in js
+
+// console.log(2 >= 2);
+// console.log(3 !== 3);
+// console.log(34 > 2);
+// console.log(4 == 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+// === check value and datatype
+console.log("1" === 1);
