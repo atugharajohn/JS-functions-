518c341088d12d7dd3ed584ee5c233eca6f1b951
atugharajohn <atugharajohn@gmail.com>
2023-10-08 16:37:06 +0530

    variables in js

--- 01_variables.js
+++ 01_variables.js
@@ -1,1 +1,19 @@
 console.log("Hello World!");
+
+// variables in js
+const accountId = 12839;
+let accountEmail = "yash@gamil.com";
+var accountPassword = "1234";
+accountCity = "Nagpur";
+let accountState;
+
+// accountId = 1834; not allowed, const values can not change
+accountEmail = "yp@23gmail.com";
+accountPassword = "2343";
+accountCity = "pune";
+
+// prefer not to used var 
+// because of issue in block scope and functional scope
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity,accountState]);
