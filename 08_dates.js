ba797c91648e7c7154328c34e07cf0390f4b127f
atugharajohn <atugharajohn@gmail.com>
2023-10-09 21:56:26 +0530

    date in js

---
+++ 08_dates.js
@@ -0,0 +1,23 @@
+// console.log(Date);
+
+// const date = new Date();
+// console.log(typeof date); // object
+// console.log(date.toString());   
+// console.log(date.toLocaleDateString()); // local date => dd/MM/yyyy
+// console.log(date.toLocaleString()); // dd/MM/yyyy, hh:mm:ss 
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getTimezoneOffset());
+// console.log(date.getDay());
+
+// const myDate = new Date("09-10-2023");
+// console.log(myDate.toLocaleString()); 
+
+// const timeStamp = Date.now();
+// console.log(timeStamp);
+// console.log(date.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
