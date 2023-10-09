9696dcbb10f40c495905775e6964ff6d1edead19
atugharajohn <atugharajohn@gmail.com>
2023-10-09 20:57:44 +0530

    string methods

--- 06_string.js
+++ 06_string.js
@@ -1,13 +1,21 @@
-const myName = "yash pal";
+const myName = "   yash pal    ";
 console.log(myName);
 
 const str2 = `mayur `;
 const str1 = new String("vinu"); // String constructor
 
-const str = 'shubhangi';
-console.log(str1);
+const str = 'sitaram';
+// console.log(str1);
 
-console.log(str.charAt(2)); // charAt => access characters
-console.log(str[2]); // another way to access characters
+// console.log(str.charAt(2)); // charAt => access an characters
+// console.log(str[2]); // another way to access characters
 
-console.log()
+console.log(str.length);
+console.log(str.split("",2)) // split into array
+console.log(str.toLowerCase()); // lowecase string
+console.log(str.toUpperCase()); // convert to uppercase
+console.log(str.replace("sita","SITA "));  // replce string
+console.log(str.indexOf('t')); // search index of character
+console.log(str.substring(0,4)); // give substring
+console.log(str.slice(1,4)); // makes slices
+console.log(myName.trim()); // remove whitespace
