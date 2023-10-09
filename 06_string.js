873e0dca9bd3893f082895f0a3a65f904e423733
atugharajohn <atugharajohn@gmail.com>
2023-10-09 20:33:06 +0530

    string functions

---
+++ 06_string.js
@@ -0,0 +1,13 @@
+const myName = "yash pal";
+console.log(myName);
+
+const str2 = `mayur `;
+const str1 = new String("vinu"); // String constructor
+
+const str = 'shubhangi';
+console.log(str1);
+
+console.log(str.charAt(2)); // charAt => access characters
+console.log(str[2]); // another way to access characters
+
+console.log()
