509fcb5f4fcb9b77b1d988c2183647f032a37b00
atugharajohn <atugharajohn@gmail.com>
2023-10-11 00:35:42 +0530

    IIFE in js

---
+++ 16_IIFE.js
@@ -0,0 +1,11 @@
+// IIFE => Immediately Invoked Functions Expressions
+
+(function code() {
+    // named IIFE
+  console.log("hello coders");
+})(); // ; is imp
+
+// (function defination)(execute)
+((name) => {
+  console.log(`databse connected ${name}`);
+})("yash");
