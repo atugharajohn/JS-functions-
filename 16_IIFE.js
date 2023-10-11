a0465bb16ec243feee1d7b0974f796779c7287d8
atugharajohn <atugharajohn@gmail.com>
2023-10-11 19:51:27 +0530

    IIFE in js

--- 16_IIFE.js
+++ 16_IIFE.js
@@ -1,5 +1,10 @@
 // IIFE => Immediately Invoked Functions Expressions
 
+// Avoid polluting the global namespace
+// Because our application could include many functions and
+//  global variables from different source files,
+//  it's important to limit the number of global variables.
+
 (function code() {
     // named IIFE
   console.log("hello coders");
