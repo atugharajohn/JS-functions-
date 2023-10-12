d5ec940aac929b19db3106ccaf0fbbd13e482972
atugharajohn <atugharajohn@gmail.com>
2023-10-12 21:04:31 +0530

    for-of loop and map in js

---
+++ 19_iterator2.js
@@ -0,0 +1,24 @@
+// for of loop
+
+// const arr = [1,2,3,4,5,6,7];
+// for (const val of arr) {
+//     console.log(val);
+// }
+
+// const str = "yash";
+// for (const val of str) {
+//     console.log(val);
+// }
+
+// Map 
+const map = new Map();
+map.set('IN',"India");
+map.set('USA',"United States of America");
+map.set('Fr',"France");
+
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key, "=" ,value);
+// }
+
